1cadf9c55c507613717cb414dc26c2f84fbd5085
rishyraj <rishyraj10@gmail.com>
2018-11-10 09:19:37 -0500

    twitch.py

---
+++ twitch.py
@@ -0,0 +1,18 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+site = 'https://overrustlelogs.net/Alexiaraye chatlog/November%202018/2018-11-09'
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+req = urllib2.Request(site, headers=hdr)
+
+page = urllib2.urlopen(req)
+
+soup = BeautifulSoup(page, 'html.parser')
+
+div_box = soup.find('div', attrs={'class':'text'})
+
